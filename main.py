33343c93fd87ae93940b7b6697d24d65d83b952d
mohammadtahaesfandairi84 <stahamohs>
2025-09-28 23:04:32 +0330

    add file

---
+++ main.py
@@ -0,0 +1,20 @@
+# def add_panj(add):
+#     tedad = 0 
+#     add_str = str(add)
+#     for x in range(len(add_str)):
+#           if add_str[x] == "5":
+#                 tedad =tedad+1
+#     return tedad
+# tedad_col = 0 
+# for i in range (60):
+#         tedad_col = tedad_col+ add_panj(i)
+# print(tedad_col)
+# x = 0
+# for z in range(21):
+#       if z == 5:
+#         x=+1       
+# print(x)
+
+
+x = ["ali","reza","taha","solmaz"]
+print(x[0[0]])
